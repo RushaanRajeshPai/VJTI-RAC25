b8e6859d378bcdd6d53fe5ce8f156c6c576aa874
Rushaan Pai <143324408+RushaanRajeshPai@users.noreply.github.com>
2024-10-10 18:23:09 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
+// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
-    build: {
-        outDir: 'dist', // Ensure the build output goes to the "dist" directory for Vercel
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    vendor: ['react', 'react-dom'],
-                    // Add more manual chunks as needed
-                },
-            },
-        },
-        chunkSizeWarningLimit: 1000, // Set a warning limit for chunk sizes in KB
-    },
-    optimizeDeps: {
-        exclude: ['.jpg', '.png', '.jpeg', '.gif'], // Exclude image files from optimization
-    },
-    assetsInclude: ['**/*.JPG'], // Include uppercase JPG files as assets
+	plugins: [react()],
 });
 

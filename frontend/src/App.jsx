4e2a9207853d62aaeb96947e0fb92f4075a28fe8
Rushaan Pai <143324408+RushaanRajeshPai@users.noreply.github.com>
2024-10-10 12:51:27 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,6 @@
-/* eslint-disable no-unused-vars */
-// import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Teams from "./pages/Teams";
-// import Forza from "./pages/Forza";
 import Motorbreath from "./pages/Motorbreath";
 import Competitions from "./pages/Competitions";
 import Activities from "./pages/Activities";
@@ -11,29 +8,24 @@ import Internships from "./pages/Internships";
 import Media from "./pages/Media";
 import Crowdfunding from "./pages/Crowdfunding";
 import SponsorUs from "./pages/SponsorUs";
-
-// Component to scroll to top whenever route changes
 import ScrollToTop from "./components/ScrolltoTop";
 
 const App = () => {
     return (
-        <div>
-            <Router>
-                <ScrollToTop />
-                <Routes>
-                    <Route element={<Home />} path="/" />
-                    <Route element={<Teams />} path="/Teams" />
-                    {/* <Route element={<Forza />} path="/Forza" /> */}
-                    <Route element={<Motorbreath />} path="/Motorbreath" />
-                    <Route element={<Competitions />} path="/Competitions" />
-                    <Route element={<Activities />} path="/Activities" />
-                    <Route element={<Internships />} path="/Internships" />
-                    <Route element={<Media />} path="/Media" />
-                    <Route element={<Crowdfunding />} path="/Crowdfunding" />
-                    <Route element={<SponsorUs />} path="/SponsorUs" />
-                </Routes>
-            </Router>
-        </div>
+        <Router>
+            <ScrollToTop />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/Teams" element={<Teams />} />
+                <Route path="/Motorbreath" element={<Motorbreath />} />
+                <Route path="/Competitions" element={<Competitions />} />
+                <Route path="/Activities" element={<Activities />} />
+                <Route path="/Internships" element={<Internships />} />
+                <Route path="/Media" element={<Media />} />
+                <Route path="/Crowdfunding" element={<Crowdfunding />} />
+                <Route path="/SponsorUs" element={<SponsorUs />} />
+            </Routes>
+        </Router>
     );
 };
 
